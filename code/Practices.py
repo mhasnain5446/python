bd7394f14606c180f15fda2c6b53dc10c316028c
Muhammad Hasnain <115575322+mhasnain5446@users.noreply.github.com>
2024-02-20 09:29:35 -0800

    Add files via upload

---
+++ code/Practices.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+name =("Hi " +input("Enter Your name Please: "))
+print(name)
+
+
+# In[15]:
+
+
+name = input("Enter your name please: ")
+lenght = len(name)
+print(name)
+print("Your Name Lenght Is ", lenght)
+
+
+# In[26]:
+
+
+name_list = []
+list_lenght = 5
+for i in range (list_lenght):
+    names = input("Enter  the names: ")
+    name_list.append(names)
+    
+print(name_list)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
